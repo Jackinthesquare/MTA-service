a76e40e0624f1dff79e217afb699578d17886239
Jacky Liang <asheriffy@gmail.com>
2022-04-01 17:39:21 -0400

    arrival times added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let div = document.getElementById('container')
-console.log(div)
+// console.log(div)
+let arrivalTimesScreen = document.getElementById('arrival-times')
 
 const getAllStations = async () => {
     let req = await fetch('https://mtaapi.herokuapp.com/stations')
@@ -8,8 +9,28 @@ const getAllStations = async () => {
         // console.log('Station',element)
     let h2 = document.createElement('h2')
     h2.innerText = element.name
+    h2.dataset.stationId = element.id
+    h2.addEventListener('click', () => {
+        getArrivalTimes(h2.dataset.stationId)
+    })
     div.append(h2)
     })
+}
+
+const displayArrivalTimes = (arrivalTimes) => {
+    arrivalTimes.forEach((element) => {
+        let h4 = document.createElement('h4')
+        h4.innerText = element
+        arrivalTimesScreen.append(h4)
+    })
+}
+
+
+const getArrivalTimes = async (stationId) => {
+    let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+    let res = await req.json()
+    console.log(res.result.arrivals)
+    displayArrivalTimes(res.result.arrivals)
 }
 
 getAllStations()
